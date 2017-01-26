44f6d60d639b03347548d437dc37a167626cd0ba
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-01-26 00:18:09 -0600

    Added possible implementation of tarai.s

--- homeworks/hw1/tarai.s
+++ homeworks/hw1/tarai.s
@@ -6,58 +6,82 @@ tarai:
     push %rbp
     mov %rsp, %rbp
 
-    # Callee-saved registers
+    mov %rsi, %rax    # Move y into RAX for return value in case of jump
+    cmp %rdi, %rsi    # Check x, y
+    jge taraiend      # if y >= x, jump
+
+    # Callee-saved registers (RBP, RBX, R12-15)
     push %r12
     push %r13
+    push %r14
 
     # Arguments:
     # x) %edi
     # y) %esi
-    # z) %edx 
-
-    mov %rdi, %r9    # Copy x to r9
-    mov %rsi, %r10   # Copy y to r10
-    mov %rdx, %r11   # Copy z to r11
+    # z) %edx
 
     # Tarai calculation
 
     # tarai(x-1, y, z)
-    # TODO: Change this to copy from r9-11 to rdi, rsi and rdx instead of moving
-    mov %rdi, %r12   # copy of x into %r12
-    add $-1, %edi    # x = x - 1
-    push %rdi        # Caller-saved register
-    push %rsi        # Caller-saved register
-    push %rdx        # Caller-saved register
+    push %rdi # Store x
+    push %rsi # Store y
+    push %rdx # Store z
 
-    call tarai       # Call recursively
-    mov %rax, %r13   # Result of tarai(x-1, y, z)
+    add $-1, %rdi   # x-1
+    call tarai      # Recursive call
 
-    pop %rdx
-    pop %rsi
-    pop %rdi
-    mov %r12, %rdi   # Restore %edi (x)
+    pop %rdx          # Restore z
+    pop %rsi          # Restore y
+    pop %rdi          # Restore x
+    mov %rax, %r12    # r12 = result 1
 
 
     # tarai(y-1, z, x)
-    mov %rsi, %r14  # Move y -> r13
-    mov %rdx, %rsi  # Move z -> y
-    mov %rdi, %rdx  # Move x -> z
-    mov %r13, %rdi  # Move y -> x
-    add $-1, %edi   # Add to y
-    push %rdi       # Caller-saved register
-    push %rsi       # Caller-saved register
-    push %rdx       # Caller-saved register
-    
-    call tarai      # Recursive call
-    mov %rax, %r14  # Result of tarai(y-1, z, x) 
+    xchg %rsi, %rdi   # (x, y, z) -> (y, x, z)
+    xchg %rdx, %rsi   # (y, x, z) -> (y, z, x)
+    push %rdi         # Store x (actually y)
+    push %rsi         # Store y (actually z)
+    push %rdx         # Store z (actually x)
 
-    pop %rdx
-    pop %rsi
-    pop %rdi
-    
+    add $-1, %rdi     # y-1
+    call tarai        # Recursive call
+
+    pop %rdx          # Restore z (actually x)
+    pop %rsi          # Restore y (actually z)
+    pop %rdi          # Restore x (actually y)
+    mov %rax, %r13    # r13 = result 2
+
+
+    # tarai(z-1, x, y)
+    xchg %rsi, %rdi   # (y, z, x) -> (z, y, x)
+    xchg %rdx, %rsi   # (z, y, x) -> (z, x, y)
+    push %rdi         # Store x (actually z)
+    push %rsi         # Store y (actually x)
+    push %rdx         # Store z (actually y)
+
+    add $-1, %rdi     # z-1
+    call tarai        # Recursive call
+
+    pop %rdx          # Restore z (actually z)
+    pop %rsi          # Restore y (actually x)
+    pop %rdi          # Restore x (actually y)
+    mov %rax, %r14    # r14 = result 3
+
+    # HERE COMES THE FUN STUFF (outer recursive call)
+    mov %r12, %rdi   # Move first result into x (RDI)
+    mov %r13, %rsi   # Move second result into y (RDI)
+    mov %r14, %rdx   # Move third result into z (RDI)
+
+    call tarai
+
+
+taraiend:
+    # Restore Callee-saved registers (RBP, RBX, R12-15)
+    pop %r14
+    pop %r13
+    pop %r12
 
     # End of function
     mov %rsp, %rbp
     pop %rbp
     ret
-
