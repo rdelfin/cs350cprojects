8473c8c0358d6018e8b5f0143d9fe592f334ef45
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-02-06 21:00:25 -0600

    Added totable.py

--- lab0/totable.py
+++ lab0/totable.py
@@ -1,16 +1,19 @@
 import csv
-with open('memory.csv', 'rt') as csvfile:
+with open('memory-format.csv', 'rt') as csvfile:
   result = ''
   reader = csv.reader(csvfile, delimiter=',', quotechar='"')
   i = 0
-  for row in reader:                                                                                                                                      
-    result += '<tr>'                                                                                                                                      
-    if(i == 0):                                                                                                                                           
-      for val in row:                                                                                                                                     
+  for row in reader:
+    result += '<tr>'
+    if(i == 0):
+      for val in row:
         result += '<th>' + str(val) + '</th>'
     else:
-      result += '<th>' + str(row[0]) + '</th>'                                                                                                            
+      result += '<th>' + str(row[0]) + '</th>'
       for val in row[1:]:
-     result += '<td>' + str(val) + '</th>'
+        result += '<td>' + str(val) + '</td>'
+    result += '</tr>\n'
     i += 1
+
+print(result)
 
