04fdac827fdd92f1ce2d5ed4cb31708226670218
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-02-08 19:57:50 -0600

    Fixed compiler errors

--- homeworks/hw3/matrix.c
+++ homeworks/hw3/matrix.c
@@ -10,14 +10,16 @@ int main(int argc, char* argv[]) {
     int i, j;
     for(i = 0; i < ROWS; i++) {
         for(j = 0; j < COLUMNS; j++) {
-            if(i == j) {
-                A[i][j] = 1;
-                B[i][j] = 1;
+            if(i == j || i + 1 == j || i - 1 == j) {
+                A[i*COLUMNS + j] = 1;
+                B[i*COLUMNS + j] = 1;
             } else {
-                A[i][j] = 0;
-                B[i][j] = 0;
+                A[i*COLUMNS + j] = 0;
+                B[i*COLUMNS + j] = 0;
             }
-            C[i][j] = 0;
+            C[i*COLUMNS + j] = 0;
         }
-    }    
+    }
+
+    
 }
