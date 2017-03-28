131c2a4bd1a48ae67acdaa3eb1bbd27a525b14b9
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-03-28 10:33:50 -0500

    Moved all instruction definitions and maps to instruction_definitions.h

---
+++ homeworks/hw6/include/instruction_definitions.h
@@ -0,0 +1,136 @@
+//
+// Created by rdelfin on 3/28/17.
+//
+
+#ifndef HW6_INSTRUCTION_DEFINITIONS_H
+#define HW6_INSTRUCTION_DEFINITIONS_H
+
+#include <unordered_map>
+
+#define HALT   0x00
+#define NOP    0x10
+#define RRMOVL 0x20
+#define CMOVLE 0x21
+#define CMOVL  0x22
+#define CMOVE  0x23
+#define CMOVNE 0x24
+#define CMOVGE 0x25
+#define CMOVG  0x26
+#define IRMOVQ 0x30
+#define RMMOVQ 0x40
+#define MRMOVQ 0x50
+#define ADDQ   0x60
+#define SUBQ   0x61
+#define ANDQ   0x62
+#define XORQ   0x63
+#define JMP    0x70
+#define JLE    0x71
+#define JL     0x72
+#define JE     0x73
+#define JNE    0x74
+#define JGE    0x75
+#define JG     0x76
+#define CALL   0x80
+#define RET    0x90
+#define PUSHQ  0xA0
+#define POPQ   0xB0
+#define IADDQ  0xC0
+#define LEAVE  0xD0
+#define NOOP   0xF0
+
+class instruction_maps {
+public:
+    static std::unordered_map<std::string, uint8_t> sizes() {
+        return {
+                {"halt",   1},
+                {"nop",    1},
+                {"noop",   1},
+                {"rrmovl", 2},
+                {"cmovle", 2},
+                {"cmovl",  2},
+                {"cmove",  2},
+                {"cmovne", 2},
+                {"cmovge", 2},
+                {"cmovg",  2},
+                {"irmovq", 10},
+                {"rmmovq", 10},
+                {"mrmovq", 10},
+                {"addq",   2},
+                {"subq",   2},
+                {"andq",   2},
+                {"xorq",   2},
+                {"jmp",    9},
+                {"jle",    9},
+                {"jl",     9},
+                {"je",     9},
+                {"jne",    9},
+                {"jge",    9},
+                {"jg",     9},
+                {"call",   9},
+                {"ret",    1},
+                {"pushq",  2},
+                {"popq",   2},
+                {"iaddq",  10},
+                {"leave",  1},
+                {"noop",   1}
+        };
+    }
+
+    static std::unordered_map<std::string, uint8_t> registers() {
+        return {
+                {"%rax", 0},
+                {"%rcx", 1},
+                {"%rdx", 2},
+                {"%rbx", 3},
+                {"%rsi", 4},
+                {"%rdi", 5},
+                {"%rsp", 6},
+                {"%rbp", 7},
+                {"%r8",  8},
+                {"%r9",  9},
+                {"%r10", 10},
+                {"%r11", 11},
+                {"%r12", 12},
+                {"%r13", 13},
+                {"%r14", 14}
+        };
+    }
+
+    static std::unordered_map<std::string, uint8_t> codes() {
+        return {
+                { "halt", HALT },
+                { "nop", NOP },
+                { "noop", NOP },
+                { "rrmovl", RRMOVL },
+                { "cmovle", CMOVLE },
+                { "cmovl", CMOVL },
+                { "cmove", CMOVE },
+                { "cmovne", CMOVNE },
+                { "cmovge", CMOVGE },
+                { "cmovg", CMOVG },
+                { "irmovq", IRMOVQ },
+                { "rmmovq", RMMOVQ },
+                { "mrmovq", MRMOVQ },
+                { "addq", ADDQ },
+                { "subq", SUBQ },
+                { "andq", ANDQ },
+                { "xorq", XORQ },
+                { "jmp", JMP },
+                { "jle", JLE },
+                { "jl", JL },
+                { "je", JE },
+                { "jne", JNE },
+                { "jge", JGE },
+                { "jg", JG },
+                { "call", CALL },
+                { "ret", RET },
+                { "pushq", PUSHQ },
+                { "popq", POPQ },
+                { "iaddq", IADDQ },
+                { "leave", LEAVE },
+                { "noop", NOOP }
+        };
+    }
+};
+
+#endif //HW6_INSTRUCTION_DEFINITIONS_H
--- homeworks/hw6/src/instruction.cpp
+++ homeworks/hw6/src/instruction.cpp
@@ -1,4 +1,5 @@
 #include <instruction.h>
+#include <instruction_definitions.h>
 
 #include <cstdint>
 #include <string>
@@ -7,33 +8,7 @@
 #include <map>
 #include <algorithm>
 
-#define HALT   0x00
-#define NOP    0x10
-#define RRMOVL 0x20
-#define CMOVLE 0x21
-#define CMOVL  0x22
-#define CMOVE  0x23
-#define CMOVNE 0x24
-#define CMOVGE 0x25
-#define CMOVG  0x26
-#define IRMOVQ 0x30
-#define RMMOVQ 0x40
-#define MRMOVQ 0x50
-#define ADDQ   0x60
-#define SUBQ   0x61
-#define ANDQ   0x62
-#define XORQ   0x63
-#define JMP    0x70
-#define JLE    0x71
-#define JL     0x72
-#define JE     0x73
-#define JNE    0x74
-#define JGE    0x75
-#define JG     0x76
-#define CALL   0x80
-#define RET    0x90
-#define PUSHQ  0xA0
-#define POPQ   0xB0
+
 
 inline bool is_whitespace(char c) {
     return c == ' ' || c == '\t' || c == '\n' || c == '\r';
@@ -130,114 +105,37 @@ instruction::instruction(std::string code, const std::map<std::string, y86addr_t
 uint8_t instruction::reg_string_to_code(std::string str) {
     long int reg = strtol(str.c_str(), nullptr, 10);
     if(isInteger(str) && reg >= 0 && reg <= 14) {
-        return reg;
+        return (uint8_t)reg;
     }
 
-    std::map<std::string, uint8_t> register_map = {
-            {"%rax", 0},
-            {"%rcx", 1},
-            {"%rdx", 2},
-            {"%rbx", 3},
-            {"%rsi", 4},
-            {"%rdi", 5},
-            {"%rsp", 6},
-            {"%rbp", 7},
-            {"%r8",  8},
-            {"%r9",  9},
-            {"%r10", 10},
-            {"%r11", 11},
-            {"%r12", 12},
-            {"%r13", 13},
-            {"%r14", 14}
-    };
 
     std::transform(str.begin(), str.end(), str.begin(), ::tolower);
 
-    if(register_map.count(str) == 0) {
+    if(instruction_maps::registers().count(str) == 0) {
         throw "Unexpected instruction";
     }
 
-    return register_map[str];
+    return instruction_maps::registers()[str];
 }
 
 uint8_t instruction::instr_string_to_size(std::string str) {
-    std::map<std::string, uint8_t> instruction_map = {
-            {"halt",   1},
-            {"nop",    1},
-            {"noop",   1},
-            {"rrmovl", 2},
-            {"cmovle", 2},
-            {"cmovl",  2},
-            {"cmove",  2},
-            {"cmovne", 2},
-            {"cmovge", 2},
-            {"cmovg",  2},
-            {"irmovq", 10},
-            {"rmmovq", 10},
-            {"mrmovq", 10},
-            {"addq",   2},
-            {"subq",   2},
-            {"andq",   2},
-            {"xorq",   2},
-            {"jmp",    9},
-            {"jle",    9},
-            {"jl",     9},
-            {"je",     9},
-            {"jne",    9},
-            {"jge",    9},
-            {"jg",     9},
-            {"call",   9},
-            {"ret",    1},
-            {"pushq",  2},
-            {"popq",   2}
-    };
 
     std::transform(str.begin(), str.end(), str.begin(), ::tolower);
 
-    if(instruction_map.count(str) == 0) {
+    if(instruction_maps::sizes().count(str) == 0) {
         throw "Unexpected instruction";
     }
 
-    return instruction_map[str];
+    return instruction_maps::sizes()[str];
 }
 
 uint8_t instruction::instr_string_to_code(std::string str) {
-    std::map<std::string, uint8_t> instruction_map = {
-            {"halt",   HALT},
-            {"nop",    NOP},
-            {"noop",   NOP},
-            {"rrmovl", RRMOVL},
-            {"cmovle", CMOVLE},
-            {"cmovl",  CMOVL},
-            {"cmove",  CMOVE},
-            {"cmovne", CMOVNE},
-            {"cmovge", CMOVGE},
-            {"cmovg",  CMOVG},
-            {"irmovq", IRMOVQ},
-            {"rmmovq", RMMOVQ},
-            {"mrmovq", MRMOVQ},
-            {"addq",   ADDQ},
-            {"subq",   SUBQ},
-            {"andq",   ANDQ},
-            {"xorq",   XORQ},
-            {"jmp",    JMP},
-            {"jle",    JLE},
-            {"jl",     JL},
-            {"je",     JE},
-            {"jne",    JNE},
-            {"jge",    JGE},
-            {"jg",     JG},
-            {"call",   CALL},
-            {"ret",    RET},
-            {"pushq",  PUSHQ},
-            {"popq",   POPQ}
-    };
 
     std::transform(str.begin(), str.end(), str.begin(), ::tolower);
 
-    if(instruction_map.count(str) == 0) {
+    if(instruction_maps::codes().count(str) == 0) {
         throw "Unexpected instruction";
-    } return instruction_map[str];
+    } return instruction_maps::codes()[str];
 }
 
 /* Individual parsers for each instruction */
