a70c7d9aaa1a17778e7f0e16fffcd574e63fd2f6
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2017-03-29 23:44:00 -0500

    Added to parser.h

--- homeworks/hw6/include/parser.h
+++ homeworks/hw6/include/parser.h
@@ -8,8 +8,18 @@
 
 class parser {
 public:
-    
+    parser(node *n);
+
+    void parse();
+
+
+
+
+    ~parser();
 private:
+    node* n;
+    std::unordered_map<std::string, y86addr_t> labelmap;    // Unordered (hash) map for fast access
+    std::map<y86addr_t, uint8_t> bytemap;                   // Ordered (tree) map for fast ordered traversal
 };
 
 
